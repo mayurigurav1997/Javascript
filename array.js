c3a3d9a3a8020812bcc46c083b988cdad644ba43
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-05-17 00:09:49 +0530

    array indexing methods

--- array.js
+++ array.js
@@ -11,3 +11,11 @@ console.log(numbers)
 
 //middle
 numbers.splice(2, 0, 'a', 'b', 'c')
+console.log(numbers)
+
+const numbers1 = [1, 2, 3, 4, 5, 1, 6, 7, 8, 1, 9];
+console.log(numbers1.indexOf('q'))  //-1
+console.log(numbers1.indexOf(3))  //2
+console.log(numbers1.lastIndexOf(1)) //9
+console.log(numbers1.indexOf(1))  //0
+console.log(numbers1.includes(1))  //true
