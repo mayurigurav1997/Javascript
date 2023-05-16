8f6102398f008749c756a7716a40d0922da91279
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-05-16 11:59:20 +0530

    String

---
+++ string.js
@@ -0,0 +1,17 @@
+const message = " This is my first message ";
+console.log(message[3])
+console.log(message.length)
+console.log(message.includes("my"))
+console.log(message.startsWith("This"))
+console.log(message.endsWith("message"))
+console.log(message.indexOf("message"))
+console.log(message.replace("first", "second"))
+console.log(message.toUpperCase())
+console.log(message.trim())
+console.log(message.trimStart())
+console.log(message.split(" "))
+
+
+const message1 = " This is my \' first message ";
+// \'
+// \n
