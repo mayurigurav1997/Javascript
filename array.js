451bb26e997a974b938cb1c538c67b05917ca15b
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-05-17 23:09:28 +0530

    removing array elements

--- array.js
+++ array.js
@@ -19,3 +19,21 @@ console.log(numbers1.indexOf(3))  //2
 console.log(numbers1.lastIndexOf(1)) //9
 console.log(numbers1.indexOf(1))  //0
 console.log(numbers1.includes(1))  //true
+
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7];
+//end
+const last = numbers2.pop()
+console.log(last)
+console.log(numbers2)
+
+//start
+const first = numbers2.shift()
+console.log(first);
+console.log(numbers2)
+
+//middle
+const middle = numbers2.splice(2, 1);
+console.log(numbers2);
+console.log(middle);
+
