c32152a502bfc77c14fab1af0b5b8d9d4cb31d54
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-05-18 09:13:58 +0530

    join and split

---
+++ join.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4]
+const join = numbers.join(",") //return string
+console.log(join)
+
+
+const message = "This is my first message"
+const parts = message.split(" ")
+console.log(parts)
+
+const combined = parts.join("-")
+console.log(combined)
