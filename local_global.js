19337c5fe5599855e42fbe8bc7df4fbeba1686f1
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-05-21 20:21:34 +0530

    global vs local

---
+++ local_global.js
@@ -0,0 +1,17 @@
+const color = "red"
+
+function start() {
+    const message = "Hi";
+    if (true) {
+        const another = 'bye';
+    }
+    // console.log(another)
+    for (let i = 0; i < 5; i++) {
+        console.log(i)
+    }
+    // console.log(i)
+    const color = 'blue'
+    console.log(color)
+}
+// console.log(message);
+start();
